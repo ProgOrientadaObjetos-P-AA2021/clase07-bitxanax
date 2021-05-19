34b410c5a44b1ad3bb256670a654d390a53243ed
bitxanax <bitxanax@gmail.com>
2021-05-19 18:26:00 -0500

    First Commit

---
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/paquete6/Hospital.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete6;
+
+public class Hospital {
+    String nombre;
+    int numeroCamas;
+    double presupuesto;
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroCamas() {
+        return numeroCamas;
+    }
+
+    public void setNumeroCamas(int numeroCamas) {
+        this.numeroCamas = numeroCamas;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+    
+}
+
+/* 
+Agregar valor de objetos de tipo Hospital a un archivo
+Y leer el archivo
+*/
